e41c83ff71db11eb639a21133756efc03333fb88
labsinet <labsinet@gmail.com>
2021-09-21 12:57:07 +0300

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+import static java.lang.Math.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=5;
+        double x=sin(a);
+        System.out.println("Hello "+a+x);
+    }
+}
